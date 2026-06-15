c1faab063d88e8f4fa6e397e7a247e3163e73a7b
Kyale33 <ronchi.kyara@pucp.edu.pe>
2026-06-14 21:32:55 -0500

    Initial commit

---
+++ Creación y enlace git.R
@@ -0,0 +1,8 @@
+#===========================================================================
+# Proyecto: Análisis de la informalidad laboral usando datos de la ENAHO
+# Autor: Kyara Ronchi
+# Fecha: 14-06
+# Objetivo del script: Creación de carpetas y enlace con GitHub
+#===========================================================================
+
+#Creamos carpetas-----------------------------
