652cab8eaccc0bb187da5c01e298e68795d2b377
Kyale33 <ronchi.kyara@pucp.edu.pe>
2026-06-15 18:55:41 -0500

    Importamos módulos de salud, educación, empleo e ingresos y gobernabilidad de la ENAHO

--- scripts/01_Carga_unión_de_módulos.R
+++ scripts/01_Carga_unión_de_módulos.R
@@ -14,3 +14,11 @@ library(readr)
 renv::snapshot(force = TRUE)
 file.exists("renv.lock")
 system("git add renv.lock -f")
+
+
+#2. Importar datos------------------------------------------------------
+
+mod300 = import("Enaho01A-2025-300.csv", encoding = "Latin-1")
+mod400 = import("Enaho01A-2025-400.csv", encoding = "Latin-1")
+mod500 = import("Enaho01a-2025-500.csv", encoding = "Latin-1")
+mod_gob = import("Enaho01B-2025-1.csv", encoding = "Latin-1")
