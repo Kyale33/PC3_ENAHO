2e582da2ed6da08c4fdb172a2e7556abb0049f81
Kyale33 <ronchi.kyara@pucp.edu.pe>
2026-06-15 00:11:37 -0500

    Creamos script para carga y unión de los módulos del ENAHO

---
+++ scripts/01_Carga_unión_de_módulos.R
@@ -0,0 +1,12 @@
+#===========================================================================
+# Proyecto: Análisis de la informalidad laboral usando datos de la ENAHO
+# Autor: Kyara Ronchi
+# Fecha: 14-06
+# Objetivo del script: Cargar los módulos y hacer los joins
+#===========================================================================
+
+#1. Carga de librerías------------------------------------------------------
+library(rio)
+library(tidyverse)
+library(janitor)
+library(readr)
